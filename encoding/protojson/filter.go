f4ac3e65e103a9870cd0e9fabcb4f2950d0f17a0
Thanh X. Pham <thanh.x.ph@gmail.com>
2023-09-20 18:06:21 +0700

    Add: support filter for marshal

---
+++ encoding/protojson/filter.go
@@ -0,0 +1,17 @@
+package protojson
+
+type fieldsFilter struct {
+	replacement string
+	fields      map[string]struct{}
+}
+
+func FieldFilter(fields []string, replacement string) fieldsFilter {
+	fieldsMap := make(map[string]struct{}, len(fields))
+	for _, f := range fields {
+		fieldsMap[f] = struct{}{}
+	}
+	return fieldsFilter{
+		replacement: replacement,
+		fields:      fieldsMap,
+	}
+}
